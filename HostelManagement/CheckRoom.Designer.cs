4f26acc7106c0ce5036b127fa656923fe26b9950
12hqwwo <quynhquynh748@gmail.com>
2025-04-22 22:10:58 +0700

    Check Room

---
+++ HostelManagement/CheckRoom.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace HostelManagement
+{
+    partial class CheckRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox4PersonRooms = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.comboBox6PersonRooms = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridViewStudents = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.studentID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mobileNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(119, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 29);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "4 - person room";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label5.Location = new System.Drawing.Point(774, 133);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 29);
+            this.label5.TabIndex = 60;
+            this.label5.Text = "6 - person room";
+            // 
+            // comboBox4PersonRooms
+            // 
+            this.comboBox4PersonRooms.BackColor = System.Drawing.Color.Transparent;
+            this.comboBox4PersonRooms.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox4PersonRooms.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4PersonRooms.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox4PersonRooms.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox4PersonRooms.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.comboBox4PersonRooms.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.comboBox4PersonRooms.ItemHeight = 30;
+            this.comboBox4PersonRooms.Items.AddRange(new object[] {
+            "101",
+            "102",
+            "103",
+            "104",
+            "105"});
+            this.comboBox4PersonRooms.Location = new System.Drawing.Point(124, 165);
+            this.comboBox4PersonRooms.Name = "comboBox4PersonRooms";
+            this.comboBox4PersonRooms.Size = new System.Drawing.Size(166, 36);
+            this.comboBox4PersonRooms.TabIndex = 61;
+            this.comboBox4PersonRooms.SelectedIndexChanged += new System.EventHandler(this.comboBox4PersonRooms_SelectedIndexChanged);
+            // 
+            // comboBox6PersonRooms
+            // 
+            this.comboBox6PersonRooms.BackColor = System.Drawing.Color.Transparent;
+            this.comboBox6PersonRooms.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox6PersonRooms.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6PersonRooms.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox6PersonRooms.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox6PersonRooms.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.comboBox6PersonRooms.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.comboBox6PersonRooms.ItemHeight = 30;
+            this.comboBox6PersonRooms.Items.AddRange(new object[] {
+            "101",
+            "102",
+            "103",
+            "104",
+            "105"});
+            this.comboBox6PersonRooms.Location = new System.Drawing.Point(779, 165);
+            this.comboBox6PersonRooms.Name = "comboBox6PersonRooms";
+            this.comboBox6PersonRooms.Size = new System.Drawing.Size(166, 36);
+            this.comboBox6PersonRooms.TabIndex = 62;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label7.Location = new System.Drawing.Point(439, 291);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 29);
+            this.label7.TabIndex = 63;
+            this.label7.Text = "ALL STUDENTS";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // dataGridViewStudents
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.dataGridViewStudents.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridViewStudents.BackgroundColor = System.Drawing.Color.Pink;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewStudents.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridViewStudents.ColumnHeadersHeight = 30;
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dataGridViewStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentID,
+            this.name,
+            this.mobileNo});
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.MidnightBlue;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewStudents.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridViewStudents.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dataGridViewStudents.Location = new System.Drawing.Point(23, 335);
+            this.dataGridViewStudents.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.RowHeadersVisible = false;
+            this.dataGridViewStudents.RowHeadersWidth = 82;
+            this.dataGridViewStudents.RowTemplate.Height = 33;
+            this.dataGridViewStudents.Size = new System.Drawing.Size(1055, 383);
+            this.dataGridViewStudents.TabIndex = 64;
+            this.dataGridViewStudents.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridViewStudents.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dataGridViewStudents.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dataGridViewStudents.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dataGridViewStudents.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dataGridViewStudents.ThemeStyle.BackColor = System.Drawing.Color.Pink;
+            this.dataGridViewStudents.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dataGridViewStudents.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dataGridViewStudents.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewStudents.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridViewStudents.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dataGridViewStudents.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dataGridViewStudents.ThemeStyle.HeaderStyle.Height = 30;
+            this.dataGridViewStudents.ThemeStyle.ReadOnly = false;
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.Height = 33;
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridViewStudents.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // studentID
+            // 
+            this.studentID.HeaderText = "Student ID";
+            this.studentID.MinimumWidth = 6;
+            this.studentID.Name = "studentID";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            // 
+            // mobileNo
+            // 
+            this.mobileNo.HeaderText = "Mobile No";
+            this.mobileNo.MinimumWidth = 6;
+            this.mobileNo.Name = "mobileNo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poor Richard", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label3.Location = new System.Drawing.Point(420, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 47);
+            this.label3.TabIndex = 65;
+            this.label3.Text = "Check Room";
+            // 
+            // CheckRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1100, 704);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox6PersonRooms);
+            this.Controls.Add(this.comboBox4PersonRooms);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CheckRoom";
+            this.Text = "CheckRoom";
+            this.Load += new System.EventHandler(this.CheckRoom_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2ComboBox comboBox4PersonRooms;
+        private Guna.UI2.WinForms.Guna2ComboBox comboBox6PersonRooms;
+        private System.Windows.Forms.Label label7;
+        private Guna.UI2.WinForms.Guna2DataGridView dataGridViewStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mobileNo;
+        private System.Windows.Forms.Label label3;
+    }
+}
